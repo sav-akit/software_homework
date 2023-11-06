5c0995d84d40e6a74f0a84c253090ca4556f4907
sav-akit <reddyssavitha@gmail.com>
2023-11-06 12:47:34 -0500

    created Hamster Class..

--- homework.js
+++ homework.js
@@ -1,2 +1,32 @@
-console.log('Hellooo');
-console.log('How are you doing?')
+// attributes:
+
+// owner - string, initially set as an empty string
+// name - string, set the name from parameter in constructor method
+// price - integer, set as 15
+// methods:
+
+// wheelRun() - log "squeak squeak"
+// eatFood() - log "nibble nibble"
+// getPrice() - return the price
+
+class Hamster {
+    constructor(name) {
+        this.owner ='';
+        this.name = name;
+        this.price = 15;
+    }
+    WheelRun() {
+        console.log("squeak squeak");
+    }
+    eatFood() {
+        console.log("nibble nibble");
+    }
+    getPrice() {
+        return this.price;
+    }
+}
+const Ham = new Hamster('Beetle');
+console.log(Ham);
+Ham.WheelRun();
+Ham.eatFood();
+console.log(Ham.getPrice());
