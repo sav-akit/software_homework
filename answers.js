042ac127f823268364d3891d85033eed8b49e7ca
sav-akit <reddyssavitha@gmail.com>
2023-11-10 22:02:12 -0500

    Methods Revisited answered

--- answers.js
+++ answers.js
@@ -47,13 +47,71 @@
 ///////////////////////////////////////////////////////////
 //Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
 
-ninjaturtles = ['Donatello','Leonardo','Raphael','Michaelangelo'];
-console.log(ninjaturtles);
+// ninjaturtles = ['Donatello','Leonardo','Raphael','Michaelangelo'];
+// console.log(ninjaturtles);
 
-//Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
+// //Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
 
-for(let item of ninjaturtles) {
-    console.log(item.toUpperCase());
-}
+// for(let item of ninjaturtles) {
+//     console.log(item.toUpperCase());
+// }
 
+//////////////////////////////////////////
+/////////////Methods, Revisited
+/////////////////////////////////////////
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 
+//Console log: the index of Titanic
+// console.log(favMovies.indexOf('Titanic'));
+// favMovies.sort();
+// //console.log(favMovies);
+
+// //use the .sortmethod Thought question: what did this do to the array? Did it permanently alter it?
+// //Yes
+// //Use the method pop
+// console.log(favMovies.pop());
+// //console.log(favMovies);
+// //push"Guardians of the Galaxy"
+
+// favMovies.push('Guardians of the Galaxy');
+// //console.log(favMovies);
+// //Reverse the array
+// favMovies.reverse();
+// //console.log(favMovies);
+// //Use the shiftmethod
+// console.log(favMovies.shift());
+// //console.log(favMovies);
+
+// //unshift- what does it return?
+// ///length of the array
+
+// console.log(favMovies.unshift('Black Panther'));
+
+//splice"Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) Thought question: did this permanently alter our array?======> Yes
+
+console.log(favMovies.indexOf('Django Unchained'));
+favMovies.splice(3,1,'Avatar');
+console.log(favMovies);
+
+//slicethe last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - Thought question: did this permanently alter our array? ===> No
+
+//store the value of your slicein a variable, console.log it - Thought question: what is going on here?
+// ====> return shollow copy
+
+console.log(favMovies.length/2);
+
+const copySlice = favMovies.slice(9);
+console.log(copySlice);
+
+console.log(copySlice.indexOf('Fast and Furious'));
+copySlice.pop();
+console.log(copySlice.indexOf('Fast and Furious'));
+
+console.log(favMovies.indexOf('Fast and Furious'));
+//After running the above tasks, console.log the index of "Fast and Furious" -We removed it from the array, what value do we get when we look for the index of something that is not in the array?======> -1
+
+//Thought question: that we declared the variable favMovieswith const, and yet, we were allowed to change the array. Weird? Should we have used let?
+//====> You can change the array but you can not reassign it
+
+const faMovies = ['sav','raj'];
+console.log(faMovies)
