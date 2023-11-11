e76a1abb8528c135651d72abc179f945ac97611a
sav-akit <reddyssavitha@gmail.com>
2023-11-10 21:07:43 -0500

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -5,6 +5,19 @@
 
 //Write a for loop that will log only the even numbers in 0 through 200.
 
-for(let i = 2; i <= 200; i += 2) {
-    console.log(i);
+// for(let i = 2; i <= 200; i += 2) {
+//     console.log(i);
+// }
+
+///////////////////////////////////////////////
+//////////Fizz Buzz
+///////////////////////////////////////////////
+for(let i =1; i <= 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    }else if(i % 3 === 0) {
+        console.log('Fizz');
+    }else if(i % 5 ===0) {
+        console.log('Buzz');
+    }
 }
